19446a09a90ff72cca484097aeb81853145d7a6a
kkevern231 <you@example.com>
2023-08-17 15:52:50 +0100

    functionality-complete--review-for-efficiency

--- build/index-script.js
+++ build/index-script.js
@@ -4,12 +4,54 @@ function selected(rating) {
 
   passParam();
 
+  //This could be its own function
+  for (let i = 1; i < 6; i++) {
+    if (i != rating)
+      //if rating != value, set reset the theme
+      switch (i) {
+        case 1:
+          rate1 = document.getElementById("1");
+          rate1.classList.remove("bg-oggy");
+          rate1.classList.remove("text-white");
+          break;
+
+        case 2:
+          rate2 = document.getElementById("2");
+          rate2.classList.remove("bg-oggy");
+          rate2.classList.remove("text-white");
+          break;
+
+        case 3:
+          rate3 = document.getElementById("3");
+          rate3.classList.remove("bg-oggy");
+          rate3.classList.remove("text-white");
+          break;
+
+        case 4:
+          rate4 = document.getElementById("4");
+          rate4.classList.remove("bg-oggy");
+          rate4.classList.remove("text-white");
+          break;
+
+        case 5:
+          rate5 = document.getElementById("5");
+          rate5.classList.remove("bg-oggy");
+          rate5.classList.remove("text-white");
+          break;
+      }
+    else {
+      continue;
+    }
+  }
+
   //const scoreSelectedElement = document.getElementById("scoreSelected");
   //scoreSelectedElement.textContent = scoreMessage;
-  switch (rating) {
+  switch (
+    rating //rating = selected number
+  ) {
     case 1:
-      rate1 = document.getElementById("1");
-      toggle(rate1);
+      rate1 = document.getElementById("1"); //create variable to work with
+      toggle(rate1); //pass element variable to toggle function
       break;
 
     case 2:
@@ -51,16 +93,9 @@ function submit() {
 }
 
 function toggle(rating) {
-  //"pop" array slot that is != to rating
-  //Then loop function to reset classes.
+  //set the new class to the selected button.
 
-  // let array = [rate1, rate2, rate3, rate4, rate5];
-
-  // for (let i = 0; i < 5; i++) {
-  //   array[i] = rating.classList.toggle("bg-medium-grey");
-  // }
-
-  rating.classList.toggle("bg-medium-grey");
+  //rating.classList.toggle("bg-medium-grey");
   rating.classList.toggle("bg-oggy");
   rating.classList.toggle("text-white");
 }
